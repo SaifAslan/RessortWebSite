6a33a16b6287e71ac66fcded0d33e70125829ed9
sDominator007 <saif.abdalkader@yahoo.com>
2020-11-03 12:33:10 +0300

    version 6.5(seperete folders)

--- app.js
+++ app.js
@@ -13,54 +13,52 @@ app.set("view engine", "ejs");
 app.use(express.static(__dirname + '/public'));
 
 
-
-
 //English Routs
 app.get("/", function(req,res){
-  res.sendFile(__dirname+"/home.html")
+  res.sendFile(__dirname+"/pageEN/home.html")
 });
 
 app.get("/contactUs",function(req,res){
-  res.sendFile(__dirname+"/contactUs.html")
+  res.sendFile(__dirname+"/pageEN/contactUs.html")
 });
 
 app.get("/aboutUs",function(req,res){
-  res.sendFile(__dirname+"/aboutUs.html")
+  res.sendFile(__dirname+"/pageEN/aboutUs.html")
 });
 app.get("/gallery", function(req,res){
-  res.sendFile(__dirname+"/gallery.html")
+  res.sendFile(__dirname+"/pageEN/gallery.html")
 });
 
 app.get("/social",function(req,res){
-  res.sendFile(__dirname+"/social.html")
+  res.sendFile(__dirname+"/pageEN/social.html")
 });
 
 app.get("/plans",function(req,res){
-  res.sendFile(__dirname+"/views/plans.html")
+  res.sendFile(__dirname+"/pageEN/plans.html")
 });
 
 //Arabic Routs
 app.get("/ar", function(req,res){
-  res.sendFile(__dirname+"/homeAR.html")
+  res.sendFile(__dirname+"/pageAR/homeAR.html")
 });
 
 app.get("/arContactUs",function(req,res){
-  res.sendFile(__dirname+"/contactUsAR.html")
+  res.sendFile(__dirname+"/pageAR/contactUsAR.html")
 });
 
 app.get("/arAboutUs",function(req,res){
-  res.sendFile(__dirname+"/aboutUsAR.html")
+  res.sendFile(__dirname+"/pageAR/aboutUsAR.html")
 });
 app.get("/arGallery", function(req,res){
-  res.sendFile(__dirname+"/galleryAR.html")
+  res.sendFile(__dirname+"/pageAR/galleryAR.html")
 });
 
 app.get("/arSocial",function(req,res){
-  res.sendFile(__dirname+"/socialAR.html")
+  res.sendFile(__dirname+"/pageAR/socialAR.html")
 });
 
 app.get("/arPlans",function(req,res){
-  res.sendFile(__dirname+"/plansAr.html")
+  res.sendFile(__dirname+"/pageAR/plansAr.html")
   // res.render(__dirname+"/views/plansAr.ejs")
 });
 
